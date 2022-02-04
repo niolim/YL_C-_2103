92debabc95121463f4212a7feb1ed22603d17029
YL <e.lugovskoy94@gmail.com>
2022-02-04 09:40:29 +0600

    Lesson_3_Done(4/4)

---
+++ Lesson 3/Lesson_3_Part_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Part_1 // Написать программу, выводящую элементы двухмерного массива по диагонали.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] MyArray = new int[,]
+            {
+               {12, 32, 54, 21 },
+               {56, 87, 76, 33 },
+               {80, 67, 58, 09 },
+               {38, 82, 91, 27 },
+            };
+            int height = MyArray.GetLength(0);
+            int width = MyArray.GetLength(1);
+
+            Console.WriteLine("Массив");
+           
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(MyArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Данные по диагонали");
+
+            for (int i=0,j=0; i<height && j<width; i++, j++)
+            {
+                Console.Write(MyArray [i,j] + "\t");
+            }
+
+        }
+    }
+}
---
+++ Lesson 3/Lesson_3_Part_2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_Part_2 
+/* Написать программу — телефонный справочник — создать двумерный массив 5*2, хранящий
+список телефонных контактов: первый элемент хранит имя контакта, второй — номер
+телефона/e-mail
+*/
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] PhoneBook = new string[5, 2];
+
+            Console.WriteLine("Введите пожалуйста Имя 1го абонента");
+            string name1 = Console.ReadLine();
+            PhoneBook[0, 0] = name1;
+            Console.WriteLine($"Введите номер телефона для абонента {name1}");
+            string number1 = Console.ReadLine();
+            PhoneBook[0, 1] = number1;
+
+
+            Console.Clear();
+
+            Console.WriteLine("Введите пожалуйста Имя 2го абонента");
+            string name2 = Console.ReadLine();
+            PhoneBook[1, 0] = name2;
+            Console.WriteLine($"Введите номер телефона для абонента {name2}");
+            string number2 = Console.ReadLine();
+            PhoneBook[1, 1] = number2;
+
+
+            Console.Clear();
+
+            Console.WriteLine("Введите пожалуйста Имя 3го абонента");
+            string name3 = Console.ReadLine();
+            PhoneBook[2, 0] = name3;
+            Console.WriteLine($"Введите номер телефона для абонента {name3}");
+            string number3 = Console.ReadLine();
+            PhoneBook[2, 1] = number3;
+
+
+            Console.Clear();
+
+            Console.WriteLine("Введите пожалуйста Имя 4го абонента");
+            string name4 = Console.ReadLine();
+            PhoneBook[3, 0] = name4;
+            Console.WriteLine($"Введите номер телефона для абонента {name4}");
+            string number4 = Console.ReadLine();
+            PhoneBook[3, 1] = number4;
+
+
+            Console.Clear();
+
+            Console.WriteLine("Введите пожалуйста Имя 5го абонента");
+            string name5 = Console.ReadLine();
+            PhoneBook[4, 0] = name5;
+            Console.WriteLine($"Введите номер телефона для абонента {name5}");
+            string number5 = Console.ReadLine();
+            PhoneBook[4, 1] = number5;
+
+
+            Console.Clear();
+
+
+
+            Console.WriteLine("Телефонный справчоник");
+            Console.WriteLine();
+            for (int y = 0; y < PhoneBook.GetLength(0); y++)
+            {
+                for (int x = 0; x < PhoneBook.GetLength(1); x++)
+                {
+                    Console.WriteLine(PhoneBook[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    } // По правде сказать, вообще не понял, как иначе можно было выполнить данное задание, поэтому получилось так топорно на мой взгляд. 
+}
